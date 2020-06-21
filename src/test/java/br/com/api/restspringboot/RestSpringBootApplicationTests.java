156894ba03a682cf61fce0e2c75fcaaaeceec981
Miguel Lima <miguelmoreiralima@outlook.com>
2020-06-21 15:13:42 -0300

    initializing the project

---
+++ src/test/java/br/com/api/restspringboot/RestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.restspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
